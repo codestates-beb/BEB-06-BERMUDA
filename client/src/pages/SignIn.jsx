a5a5e5801476378fb8e501f5f3413c69696b2bf9
copa74 <copa74@naver.com>
2022-11-01 20:08:46 +0900

    feat: 회원가입 로그인 기능 추가

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,20 @@
 import { Fragment , useState } from "react";
 import Signup from "../components/Signup";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function SiginIn() {
+function SiginIn(props) {
     const [ signinOepn , setSigninOepn ] =  useState(false);
+    const [ id ,setId ] = useState();
+    const [ password ,setPassword ] = useState();
+
+    const onChangeId = (e) => {
+      setId(e.Target.value);
+    }
+
+    const onChangePassword = (e) => {
+      setPassword(e.Target.value);
+    }
 
     const onOpenSignIn = () => {
       setSigninOepn(true);
@@ -10,6 +22,32 @@ function SiginIn() {
 
     const onCloseSignIn = () => {
       setSigninOepn(false);
+    }
+
+    const navigate = useNavigate();
+
+    const onSignIn = () => {
+
+      const signIn = {
+        user_id: id,
+        password: password,
+      };
+
+        axios.post('http://localhost:8080/user/login', {signIn})
+        .then(function(res){
+          props.setUserData(res.data);
+          props.setLogin(true);
+          alert("로그인 성공!");
+
+         
+          navigate("/");
+          
+
+        }).catch(function (error) {
+          console.log(error);
+        });
+      
+
     }
 
 
@@ -20,20 +58,20 @@ function SiginIn() {
             <div className="login_title" >로그인</div>
 
             <div className="relative">
-              <input className="login_input" placeholder="아이디" ></input>
+              <input className="login_input" placeholder="아이디" onChange={onChangeId} value={id} ></input>
               <span className="material-symbols-outlined">
                   person
               </span>
             </div>
 
             <div  className="relative">
-              <input className="login_input" placeholder="비밀번호"  ></input>
+              <input className="login_input" placeholder="비밀번호"  onChange={onChangePassword} value={password}   ></input>
               <span className="material-symbols-outlined">
               lock
               </span>
             </div>
 
-            <div className="login_button" >로그인</div>
+            <div className="login_button" onClick={onSignIn} >로그인</div>
             <div className="sign_up_page_text" onClick={onOpenSignIn}  > 아이디가 없다면 <span style={{ cursor : "pointer" , color:"#343434" }}  >회원가입</span>  </div>
           </div>
         </div>
